71c8e81e57f069b25ea1d32474432ab78ffc3f66
FredrikLindgren <lindgren.fredrik@gmail.com>
2014-08-18 20:31:45 +0200

    Traify kpglai01.tra

--- randomiser/d/kpglai01.d
+++ randomiser/d/kpglai01.d
@@ -1,6 +1,6 @@
 APPEND kpglai01
   IF ~~ flaildesc
-    SAY ~He said it had powers of flame and acid and the like...things that would have aided us greatly against the trolls, had we found them in time.~
+    SAY @0
       IF ~!HasItem("blun14a",Myself) !HasItem("blun14b",Myself) !HasItem("blun14c",Myself)~  GOTO noflail
       IF ~HasItem("blun14a",Myself) !HasItem("blun14b",Myself) !HasItem("blun14c",Myself)~  GOTO onehead
       IF ~!HasItem("blun14a",Myself) HasItem("blun14b",Myself) !HasItem("blun14c",Myself)~  GOTO onehead
@@ -12,39 +12,39 @@ APPEND kpglai01
   END
 
   IF ~~ noflail
-    SAY ~But we were captured before we had found any of the flail heads.~
+    SAY @1
       IF ~~  GOTO noflail2
   END
 
   IF ~~ noflail2
-    SAY ~If you find the flail heads, you could assemble the weapon.~
-    =   ~Lord de'Arnise and I were heading to the hidden forge.  He said it was behind a secret door in the armory on the keep's first floor.  That might be a good place to look.~
+    SAY @2
+    =   @3
       IF ~~  GOTO 7
   END
 
   IF ~~ onehead
-    SAY ~I...have one of the flail heads, still. Here, you may have it.~
+    SAY @4
       IF ~~  DO ~GiveItem("blun14a",LastTalkedToBy) GiveItem("blun14b",LastTalkedToBy) GiveItem("blun14c",LastTalkedToBy)~  GOTO 9
   END
   
   IF ~~ twohead
-    SAY ~We had found two of the heads before we were captured. I still have them, it seems. Here, you may have them.~
+    SAY @5
       IF ~~  DO ~GiveItem("blun14a",LastTalkedToBy) GiveItem("blun14b",LastTalkedToBy) GiveItem("blun14c",LastTalkedToBy)~  GOTO twohead2
   END
   
   IF ~~ twohead2
-    SAY ~If you find the last flail head, you could assemble the weapon.~
-    =   ~Lord de'Arnise and I were heading to the hidden forge.  He said it was behind a secret door in the armory on the keep's first floor.  That might be a good place to look.~
+    SAY @6
+    =   @3
       IF ~~  GOTO 7
   END
   
   IF ~~ luckybastard
-    SAY ~I remember...we had found all three flail heads and were heading for the forge when we were captured.  It's amazing I still have them. Here, take them.~
+    SAY @7
       IF ~~  DO ~GiveItem("blun14a",LastTalkedToBy) GiveItem("blun14b",LastTalkedToBy) GiveItem("blun14c",LastTalkedToBy)~  GOTO luckybastard2
   END
   
   IF ~~ luckybastard2
-    SAY ~You should go to the forge and assemble the weapon.  Lord de'Arnise said it was hidden behind a secret door in the armory on the keep's first floor.~
+    SAY @8
       IF ~~  GOTO 7
   END
 END
